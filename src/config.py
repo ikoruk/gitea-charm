0ecafe31e9a1ba9afec75e41f238fa8f99086e6a
Jacob Martin <jacob.martin@canonical.com>
2023-12-06 15:37:04 -0600

    Minimize Gitea app.ini and leave install lock disabled
    Signed-off-by: Jacob Martin <jacob.martin@canonical.com>

--- src/config.py
+++ src/config.py
@@ -37,10 +37,16 @@ class GiteaConfig():
         with open(self._path, "w+") as file:
             self._config.write(file)
 
+    def _ensure_section(self, section: str):
+        if not self._config.has_section(section):
+            self._config.add_section(section)
+
     def _gitea_server_port(self, config: ops.model.ConfigData):
+        self._ensure_section('server')
         self._config.set('server', 'HTTP_PORT', str(config['gitea-server-port']))
 
     def _gitea_oauth2_jwt_secret(self, config: ops.model.ConfigData):
+        self._ensure_section('server')
         key = 'gitea-oauth2-jwt-secret'
         if key in config:
             self._config.set('oauth2', 'ENABLE', 'true')
@@ -49,24 +55,44 @@ class GiteaConfig():
             self._config.set('oauth2', 'ENABLE', 'false')
 
     def _gitea_proxy_url(self, config: ops.model.ConfigData):
+        self._ensure_section('proxy')
         self._config.set('proxy', 'PROXY_URL', config['gitea-proxy-url'])
 
     def _gitea_proxy_hosts(self, config: ops.model.ConfigData):
+        self._ensure_section('proxy')
         self._config.set('proxy', 'PROXY_HOSTS', config['gitea-proxy-hosts'])
 
     def _gitea_storage_dir(self, config: ops.model.ConfigData):
         dir = config['gitea-storage-dir']
+
+        self._ensure_section('repository')
         self._config.set('repository', 'ROOT', f"{dir}/repos")
+
+        self._ensure_section('picture')
         self._config.set('picture', 'AVATAR_UPLOAD_PATH', f"{dir}/avatars")
         self._config.set('picture', 'REPOSITORY_AVATAR_UPLOAD_PATH', f"{dir}/repo-avatars")
+
+        self._ensure_section('attachment')
         self._config.set('attachment', 'PATH', f"{dir}/attachments")
+
+        self._ensure_section('packages')
         self._config.set('packages', 'PATH', f"{dir}/packages")
+
+        self._ensure_section('storage')
         self._config.set('storage', 'PATH', f"{dir}/repo-archive")
+
+        self._ensure_section('storage.packages')
         self._config.set('storage.packages', 'PATH', f"{dir}/packages")
+
+        self._ensure_section('storage.actions_log')
         self._config.set('storage.actions_log', 'PATH', f"{dir}/actions_log")
+
+        self._ensure_section('storage.actions_artifacts')
         self._config.set('storage.actions_artifacts', 'PATH', f"{dir}/actions_artifacts")
 
     def _gitea_metrics_token(self, config: ops.model.ConfigData):
+        self._ensure_section('metrics')
+
         key = 'gitea-metrics-token'
         if key in config:
             self._config.set('metrics', 'ENABLED', 'true')
@@ -75,6 +101,8 @@ class GiteaConfig():
             self._config.set('metrics', 'ENABLED', 'false')
 
     def set_db_config(self, username: str, password: str, host: str):
+        self._config.set('database', 'DB_TYPE', 'postgres')
+        self._config.set('database', 'SCHEMA', '')
         self._config.set('database', 'NAME', "giteadb")
         self._config.set('database', 'USER', username)
         self._config.set('database', 'PASSWD', password)
